ef18f677ad1834e4db01358853b86d6c7be1aced
luismen06 <luissierradiaz5604@gmail.com>
2026-01-23 18:27:41 -0500

    cliente cambios para en axiosconfig.js para render

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // Si existe la variable de entorno (Nube), úsala. Si no, usa localhost.
-const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:3000/api';
+const baseURL = import.meta.env.VITE_API_URL || 'https://celestial-servicios-react.onrender.com';
 
 const api = axios.create({
     baseURL: baseURL,
